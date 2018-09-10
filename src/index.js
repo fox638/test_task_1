ec433d568426ab88872729cb121c128b5928a06e
Dima <fox6383@gmail.com>
2018-09-10 14:07:07 +0300

    add router redux and reducer news

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import {ConnectedRouter} from 'connected-react-router'
+import store from './store'
+import history from './history'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render((
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App />
+        </ConnectedRouter>
+        
+    </Provider>
+    ), document.getElementById('root'));
+
